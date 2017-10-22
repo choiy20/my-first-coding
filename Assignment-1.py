94afa37c5243cb5532264e6661e72cd48348aa2d
choiy20 <yoojin.kelly.choi@gmail.com>
2017-10-22 09:54:56 +0800

    Update Assignment-1.py

--- Assignment-1.py
+++ Assignment-1.py
@@ -1,19 +1,19 @@
-Restaurant = str(input("Please choose one from Korean, Chinese or Japanese: "))
+restaurant = str(input("Please choose one from Korean, Chinese or Japanese: "))
 
 
 import random
 
-if Restaurant in ["Korean", "korean", "kor", "Korea", "korea"]:
-    Korean = ["K1", "K2", "K3", "K4", "K5"]
-    print (random.choice(Korean))
+if restaurant in ["Korean", "Korean".lower(), "Korea", "Korea".lower()]:
+    korean = ["K1", "K2", "K3", "K4", "K5"]
+    print (random.choice(korean))
 
-elif Restaurant in ["Chinese", "chinese", "chi", "China", "china"]:
-    Chinese = ["C1", "C2", "C3", "C4", "C5"]
-    print (random.choice(Chinese))
+elif restaurant in ["Chinese", "Chinese".lower(), "China", "China".lower()]:
+    chinese = ["C1", "C2", "C3", "C4", "C5"]
+    print (random.choice(chinese))
 
-elif Restaurant in ["Japanese", "japanese", "jap", "Janpan", "japan"]:
-    Japanese = ["J1", "J2", "J3", "J4", "J5"]
-    print (random.choice(Japanese))
+elif restaurant in ["Japanese", "Japanese".lower(), "Janpan", "Japan".lower()]:
+    japanese = ["J1", "J2", "J3", "J4", "J5"]
+    print (random.choice(japanese))
 
 else:
     print ("Not valid. Please choose one from Korean, Chinese or Japanese")
